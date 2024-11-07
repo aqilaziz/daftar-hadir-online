f56861691bacced582c7c5895f7a4fb8f8c8734d
Aqil Aziz <46887634+aqilaziz@users.noreply.github.com>
2024-11-07 12:13:04 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.getElementById("attendanceForm").addEventListener("submit", async (e) =
     };
 
     try {
-        const result = await fetch("https://script.googleusercontent.com/macros/echo?user_content_key=NoSxD7r2dor4d_VEGUd_jyd9uRl-iaP3erS1CRSK7oNbX_f1uPMUYJ3VaQ8q0kt1guCxJZkz_4XzhtOS-aaikBI0CfiAGiobm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnF_SKJJZ6ZgVdwxSc2qYA1aCJDpg3iYaXRQJagp4rxnHr3nmy8YMJ3eNBxp6d4AOV_KKtCsKOad2Tu0udVi_D3bK0wieJivUtNz9Jw9Md8uu&lib=MNySORo3_MV1rCkrR4tlmVYY0_T-l_3Eo", {
+        const result = await fetch("https://script.google.com/macros/s/AKfycbxgcTcjSpDw_XFAFELGBL5qyvSzeoGPr5uOWZklmZF5hlYDSeEXcr1LaCjoRF36hWpcsQ/exec", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
